0f8275abda0f52ab7c61647fb8a623d58d590021
铂屑 <1824390830@qq.com>
2022-05-01 01:20:49 +0800

    改名bug修复
    key值更正为UID，添加id_index字典，修复改名bug。

--- gift_get.py
+++ gift_get.py
@@ -42,6 +42,7 @@ def year_month():
 def gift_get(session, year, month):
     date_list = get_date_list(year, month)
     result = {}
+    id_index = {}
     for date in date_list:
         all_info = session.get(
             url="https://api.live.bilibili.com/xlive/revenue/v1/giftStream/getReceivedGiftStreamNextList",
@@ -59,7 +60,8 @@ def gift_get(session, year, month):
         gift_num = len(gifts)
 
         for i in range(gift_num):
-            key = gifts[i]["uname"] + " (" + str(gifts[i]["uid"]) + ")"
+            key = str(gifts[i]["uid"])
+            id_index[key] = gifts[i]["uname"]
             if gifts[i]["gift_name"] in ("舰长", "提督", "总督"):
                 title = gifts[i]["gift_name"]
             else:
@@ -70,19 +72,19 @@ def gift_get(session, year, month):
             else:
                 val = {"总督": [], "提督": [], "舰长": [], title: [gifts[i]["time"]]}
                 result[key] = val
-    return result
+    return result, id_index
 
 
 def generate_gift_file(session):
     year, month = year_month()
-    result_dict = gift_get(session, year, month)
-    gift_file_xls(result_dict)
+    result_dict, id_index = gift_get(session, year, month)
+    gift_file_xls(result_dict, id_index)
     aige_score_calc(session)
     nowdir = os.getcwd()
     result_file = os.path.join(nowdir, "统计结果.txt")
     file = open(result_file, "w", encoding="utf-8")
     for usr in result_dict:
-        line = "========== " + usr + " 的大航海 ==========\n"
+        line = "========== " + id_index[usr] + "(" + usr + ") 的大航海 ==========\n"
         for title in result_dict[usr]:
             all_dates = result_dict[usr][title]
             if len(all_dates) == 0:
@@ -94,32 +96,36 @@ def generate_gift_file(session):
     file.close()
 
 
-def gift_file_xls(gift_dict):
+def gift_file_xls(gift_dict, id_index):
     wb = xlwt.Workbook()
     sheet = wb.add_sheet('大航海统计')
-    sheet.write(0, 0, 'ID')
-    sheet.write(0, 1, '大航海类型')
-    sheet.write(0, 2, '时间')
+    sheet_header_list = ['ID', 'UID', '大航海类型', '时间']
+    for i in range(len(sheet_header_list)):
+        sheet.write(0, i, sheet_header_list[i])
     row = 1
+
     sheet1 = wb.add_sheet('积分计算')
-    sheet1_head = ['ID', '当月积分', '舰长', '提督', '总督']
+    sheet1_head = ['ID', 'UID', '当月积分', '舰长', '提督', '总督']
     for i in range(len(sheet1_head)):
         sheet1.write(0, i, sheet1_head[i])
     row1 = 1
+
     for usr in gift_dict:
-        sheet.write(row, 0, usr)
-        sheet1.write(row1, 0, usr)
+        sheet1.write(row1, 1, usr)
+        sheet1.write(row1, 0, id_index[usr])
         scores = len(gift_dict[usr]['舰长']) + 15 * len(gift_dict[usr]['提督']) + 200 * len(gift_dict[usr]['总督'])
-        sheet1.write(row1, 1, scores)
+        sheet1.write(row1, 2, scores)
         for title in gift_dict[usr]:
             all_dates = gift_dict[usr][title]
             column = sheet1_head.index(title)
             sheet1.write(row1, column, len(all_dates))
             if len(all_dates) == 0:
                 continue
-            sheet.write(row, 1, title)
             for date in all_dates:
-                sheet.write(row, 2, date)
+                sheet.write(row, 1, usr)
+                sheet.write(row, 0, id_index[usr])
+                sheet.write(row, 2, title)
+                sheet.write(row, 3, date)
                 row += 1
         row1 += 1
     wb.save("大航海统计.xls")
@@ -129,6 +135,7 @@ def aige_score_calc(session):
     now_year = datetime.datetime.today().year
     now_month = datetime.datetime.today().month
     score_dict = {}
+    id_index_total = {}
     date_str_list = []
     for year in range(2021, now_year + 1):
         if year == 2021:
@@ -141,7 +148,8 @@ def aige_score_calc(session):
         for month in range(begin_month, end_month + 1):
             date_str = "-".join((str(year), str(month)))
             date_str_list.append(date_str)
-            gift_dict = gift_get(session, year, month)
+            gift_dict, id_index = gift_get(session, year, month)
+            id_index_total.update(id_index)
             for usr, value in gift_dict.items():
                 scores = len(gift_dict[usr]['舰长']) + \
                          15 * len(gift_dict[usr]['提督']) + \
@@ -154,18 +162,20 @@ def aige_score_calc(session):
 
     wb = xlwt.Workbook()
     sheet = wb.add_sheet('艾鸽积分')
-
+    sheet.write(0, 0, "ID")
+    sheet.write(0, 1, "UID")
     for i in range(len(date_str_list)):
-        sheet.write(0, i + 1, date_str_list[i])
+        sheet.write(0, i + 2, date_str_list[i])
 
     row = 1
     for usr in score_dict:
-        sheet.write(row, 0, usr)
+        sheet.write(row, 1, usr)
+        sheet.write(row, 0, id_index_total[usr])
         for i in range(len(date_str_list)):
             if date_str_list[i] in score_dict[usr]:
-                sheet.write(row, i + 1, score_dict[usr][date_str_list[i]])
+                sheet.write(row, i + 2, score_dict[usr][date_str_list[i]])
             else:
-                sheet.write(row, i + 1, 0)
+                sheet.write(row, i + 2, 0)
         row += 1
 
     wb.save("艾鸽积分.xls")
