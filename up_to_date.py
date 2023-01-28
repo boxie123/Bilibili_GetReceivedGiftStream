342535f495d2d8931d46068a7fb5526fdadec299
铂屑 <80164656+boxie123@users.noreply.github.com>
2023-01-29 02:15:19 +0800

    💄 Beautify the CLI

--- up_to_date.py
+++ up_to_date.py
@@ -1,21 +1,32 @@
 import httpx
 
+from console import console
+
 
 def main(now_tag):
-    print("\n\n开始检测更新（此步骤不影响上述已生成的文件，若无需检测可直接关闭程序）")
+    console.print("\n\n")
+    console.rule("检测更新")
     header = {"accept": "application/vnd.github.v3+json"}
     url = (
         "https://api.github.com/repos/boxie123/Bilibili_GetReceivedGiftStream/releases"
     )
     param = {"per_page": 1, "page": 1}
-    releases_info = httpx.get(url, headers=header, params=param, timeout=None, verify=False).json()
-    tag_name = releases_info[0]["tag_name"]
-    if tag_name == now_tag:
-        print("提示：当前已是最新版本")
-    else:
-        new_html_url = releases_info[0]["html_url"]
-        print("提示：检测到更新，当前版本{}，最新版本{}".format(now_tag, tag_name))
-        print("请复制以下链接到浏览器下载最新版本：{}".format(new_html_url))
+    with console.status("正在检测更新（此步骤不影响上述已生成的文件，若无需检测可直接关闭程序）", spinner="line"):
+        releases_info = httpx.get(
+            url, headers=header, params=param, timeout=None, verify=False
+        ).json()
+        tag_name = releases_info[0]["tag_name"]
+        if tag_name == now_tag:
+            console.print("提示：当前已是最新版本")
+        else:
+            new_html_url = releases_info[0]["html_url"]
+            console.print(
+                "提示：检测到更新，当前版本[blue]{}[/blue]，最新版本[green]{}[/green]".format(
+                    now_tag, tag_name
+                )
+            )
+            console.print("\n可点击下面的链接到浏览器下载最新版本：")
+            console.print(new_html_url, style=f"link {new_html_url}")
 
 
 # if __name__ == "__main__":
